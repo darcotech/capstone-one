9dd5093eeaad3f62c3b8e34df6133a142717c74e
Darko Sola <dsola@darcotech.com>
2023-04-28 01:14:08 +0200

    Hopefully final one

--- main.tf
+++ main.tf
@@ -1,66 +1,125 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
+# provider section
+variable "access_key" {}
+variable "access_secret" {}
+variable "public_key" {}
+#variable "private_key_pem" {}
 
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.access_secret
 }
+
+# VPC section
+#resource "aws_vpc" "hg-vpc" {
+#  cidr_block = "10.0.0.0/16"
+#  tags = {
+#    Name = "hangout-web"
+#  }
+#}
+
+#resource "aws_subnet" "subnet-0" {
+#  vpc_id            = aws_vpc.hg-vpc.id
+#  cidr_block        = "10.0.1.0/24"
+
+#  tags = {
+#    Name = "hangout prod subnet"  }
+#}
+
+# End of VPC section
+
+# Security group section
 
 resource "aws_security_group" "allow_SSH" {
   name        = "allow_SSH"
   description = "Allow SSH inbound traffic"
-  #   vpc_id      = aws_vpc.main.id
+  #vpc_id      = aws_vpc.default
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "HTTP-Jen"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+   description = "HTTP-Docker-API"
+   from_port   = 4200
+   to_port     = 4400
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ ingress {
+  description = "HTTP-Docker-hostport"
+  from_port   = 32768
+  to_port     = 60999
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
 
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-    # description      = "SSH from VPC"
-    # from_port        = 22
-    # to_port          = 22
-    # protocol         = "tcp"
-    # cidr_blocks      = ["61.6.14.46/32"]
-    # # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
   }
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDL2g8Pyo/WdFXw001NsUcoa1qfNmCyVZW/IfA2wMPDG3R3QbLtGLgJ17lVXmGFsRseu3SPhsS3fDdG6PwlZo26PFvnZtSO+I92eJQqwfAq//d4epsrJMY82jWDmAoTxUuxFZ2ob11ohvqU59l6xtJp+NDl0rpd+/Pn7329HuB/vXlb4AtHNzdhWoeT9Tb1Wd4JVmZjf6YgRZl/NxyKEkCl/LKDRQCfeXGoQIqYLgSU09//Ll1xLp5Gh/Wdh8Pk1GLbGwGKgHsmOwsipeCd4ul8gOrt99gv2K31OWxCWZUkh2xKT9lOOrZ958uNZCVswD5lgwWmzZ5Q3jbhXti+LjrBuj9Ri+4fsleyjE7IWHvche2qz/CNQAR30CroDfmwo3KCj4XP0fyGgq2U0nAA+QRQqorZZSyJ09Nzl6QEHhNhMpufoeW426JhMHgSiJ+G8aAU68BAQj9VYxNt7N+bhubQKAicVXKqoLfzROZOJ6F2OiZ69D6ZkTKnt0TNHPpWIpc= dsoladarcotech@ip-172-31-31-29"
-}
+# End of security group section
+
+
+# Jenkins instance creation section
+
+resource "aws_key_pair" "deployer4" {
+  key_name   = "deployer4"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLbr9ATK0QLXbQb4apk2iX2/XT8Hq9ON1XQZWJQlnjIvP5QBWryUy2kpfJJgfLJ5FC3wl3OGA9Y/4lDBlwCd0+SRA+k+tL7F5J6A8Nu42qvKeWPVYlgEz5lkNhFZcpJazKP0vcVTGfj2So0iGU9RQqGgH2AU04ncIXuO/0DErvDx7ZXYcEtrV3c+3yasgdJFkS0sTJ4eMsEwHp0e8eHD1k4xYo69RK+6FUYfu4WzTRwSteDuxJG06zO/s0Sh9J/SoyN43R+TDpw5jXVsjwcF+p66rst3RgGbSTC4+HuhyyPKN8piEHQCOeQ4o8mzRL6yy84q4JoD79IkL6JNgzh+19A/mXHxm5uGVIjhgk48by0UItQUDwziruhn9zYcHz6CGcqgylhCXR+zbYJYmtqO4qb7gQd0ROZbmDI8+vc2qTrhpT39DbmSOwAR54nklMNIL5WeXJy9M3lywgbUVgYUm1pL2oG/O0SMl66Ncd3lxK8eIVtfQPmu45e0RaagdaUSE= dsc@pop-os"
+  }
 
 
 
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-04505e74c0741db8d"
+resource "aws_instance" "jenkins-controller" {
+  ami           = "ami-0aa2b7722dc1b5612"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.deployer4.key_name
+  #vpc_id      = aws_vpc.hg-vpc.id
   vpc_security_group_ids = ["${aws_security_group.allow_SSH.id}"]
   tags = {
-    "Name" = "UBUNTU-Node"
-    "ENV"  = "Dev"
+    "Name" = "Jenkins Controller"
+    "ENV"  = "Prod"
   }
 
-  depends_on = [aws_key_pair.deployer]
+  depends_on = [aws_key_pair.deployer4]
 
   # Type of connection to be established
   connection {
@@ -73,15 +132,62 @@ resource "aws_instance" "ubuntu" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt update && upgrade",
-      "sudo apt install -y python3.8",
-      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ >  /etc/apt/sources.list.d/jenkins.list'",
+      "echo starting installation from NOW",
+      "sudo wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -",
+      "sudo add-apt-repository 'deb https://apt.corretto.aws stable main'",
       "sudo apt-get update",
-      "sudo apt-get install -y openjdk-8-jre",
+      "sudo apt-get install -y java-17-amazon-corretto-jdk",
+      "echo JAVA installed",
+      "sudo apt-get install -y ttf-dejavu",
+      "sudo apt-get install -y fontconfig",
+      "sudo apt install -y python3.8",
+      "echo PYHTON installed",
+      "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
+      "sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null'",
+      "sudo apt-get update",
       "sudo apt-get install -y jenkins",
+      "echo JENKINS installed",
+      #"sudo apt install net-tools",
+      "sudo hostnamectl set-hostname jenkins-c",
+      #"ifconfig",
     ]
   }
+}
 
-  
 
+
+resource "aws_instance" "docker-node" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer4.key_name
+  #vpc_id      = aws_vpc.hg-vpc.id
+  vpc_security_group_ids = ["${aws_security_group.allow_SSH.id}"]
+  tags = {
+    "Name" = "Docker Node"
+    "ENV"  = "Prod"
+  }
+depends_on = [aws_key_pair.deployer4]
+    # Type of connection to be established
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./deployer")
+    host        = self.public_ip
+  }
+  # Remotely execute commands to install Java, Python, Jenkins
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -",
+      "sudo add-apt-repository 'deb https://apt.corretto.aws stable main'",
+      "sudo apt-get update",
+      "sudo apt-get install -y java-17-amazon-corretto-jdk",
+      "curl -fsSL https://get.docker.com -o get-docker.sh",
+      "sudo sh get-docker.sh",
+      #"sudo apt install net-tools",
+      "sudo hostnamectl set-hostname docker-node",
+      "echo 42",
+      #"ifconfig",
+    ]
+  }
 }
